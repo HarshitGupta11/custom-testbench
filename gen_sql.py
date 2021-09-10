38c7cffcca085582a26d2c1ee64b69fe0ca36fb3
HarshitGupta11 <gupta.harshit231199@gmail.com>
2021-09-10 15:59:34 +0530

    Some major changes

--- gen_sql.py
+++ gen_sql.py
@@ -54,29 +54,37 @@ def gen_prep_fact(tblname, num_parts, num_prim_key, rand_width):
 
 def gen_sql_dim_table(tablename, file):
     file.write(drop_dim_table_template.format(tablename))
+    file.write("\n")
     cols = gen_prep_dim(tablename)
     file.write(create_table_template.format(tablename, cols))
+    file.write("\n")
     file.write(load_data_template.format(tablename, tablename))
+    file.write("\n")
     return
 
 
 def gen_sql_dim_table_orc(tblname, file):
     col_names = gen_prep_dim(tblname)
     file.write(create_table_template_dim_orc.format(tblname, col_names))
+    file.write("\n")
     return
 
 
 def gen_sql_dim_table_overwrite(tblname, orctblname, file):
     file.write(insert_dim_stmt_template.format(orctblname, tblname))
+    file.write("\n")
     return
 
 
 def gen_sql_fact_table(tablename, num_parts, num_prim, rand_width,file):
     file.write(drop_dim_table_template.format(tablename))
+    file.write("\n")
     # generate the create table stmt
     # generate the column names
     file.write(create_table_template.format(tablename, gen_prep_fact(tablename, num_parts, num_prim, rand_width)))
+    file.write("\n")
     file.write(load_data_template.format(tablename, tablename))
+    file.write("\n")
     return
 
 
@@ -97,6 +105,7 @@ def gen_sql_fact_table_orc(tblname, num_parts, num_prim, rand_width, file):
     fin_col = ",".join(ls_cols)
     fin_part_col = ",".join(ls_parts)
     file.write(create_table_template_fact_orc.format(tblname, fin_col, fin_part_col))
+    file.write("\n")
     return
 
 
@@ -108,4 +117,5 @@ def gen_sql_fact_table_partitioned_load(tblname, orctblname, num_parts,file):
         ls_parts.append(part_temp.format(tblname, i + 1))
     fin_part = ",".join(ls_parts)
     file.write(insert_fact_stmt_template.format(orctblname, fin_part, tblname))
+    file.write("\n")
     return
